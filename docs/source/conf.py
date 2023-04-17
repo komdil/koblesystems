58ade178e46ed533b618cba384f685ea451dd7b3
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-04-17 13:43:06 +0500

    import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
+import sphinx_rtd_theme
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
